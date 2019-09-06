9de10bf5e1de6ff371466f4563b44fed7917a630
Jonathan Campbell <jonathan@castus.tv>
2019-09-05 18:26:13 -0700

    more

--- make-compat-chart.pl
+++ make-compat-chart.pl
@@ -71,15 +71,6 @@ while ($line = <S>) {
 
     next unless -d $line;
 
-    # skip Amiga demos, we can't run them
-    next if $line =~ m/\/amiga\/(demo|in64|in40|in4k)\//;
-
-    # no MOD music executables
-    next if $line =~ m/\/m4ch\//;
-
-    # no graphics
-    next if $line =~ m/\/grfx\//;
-
     # some are symlinks
     $symlink_to = undef;
     if ( -l $line ) {
@@ -89,7 +80,6 @@ while ($line = <S>) {
 
     $line =~ s/^\.\///;
     $disp_line = $line;
-    next unless $disp_line =~ s/^unpacked\///;
 
     my $pass_dosbox_x = undef;
     my $pass_dosbox_x_rev = undef;
