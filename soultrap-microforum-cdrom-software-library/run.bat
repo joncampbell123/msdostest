96e14299efc92756755430ec9fdd814567fdb2a8
Jonathan Campbell <jonathan@castus.tv>
2019-12-30 15:23:24 -0800

    more

--- soultrap-microforum-cdrom-software-library/run.bat
+++ soultrap-microforum-cdrom-software-library/run.bat
@@ -1,3 +1,12 @@
 @echo off
+if exist c:\progra~1\microf~1\soultrap\soultrap.exe goto skipinst
+echo Running install.
+echo Remember to ask for minimal install to run off of CD-ROM
+echo and let it install to the default location. When prompted
+echo to install DirectX, decline. DirectX is already installed.
+pause
 d:\setup.exe
+pause
+exit
+:skipinst
 
